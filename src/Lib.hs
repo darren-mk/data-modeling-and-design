692bbecf0ebfde6662eb395a0223df2d6053ea37
Darren Kim <darren.minsoo.kim@gmail.com>
2025-02-14 18:43:25 +0900

    design domain modeling

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,33 @@ module Lib
     ( someFunc
     ) where
 
+data MaritalStatus
+    = Single
+    | Married
+    | Divorced
+    | Widowed
+    | Separated
+
+type FirstName = String
+type LastName = String
+type SocialSecurityNumber = String
+type PhoneNumber = String
+
+data PersonInEditing =
+  PersonInEditing
+  { firstNameStr :: Maybe String
+  , lastNameStr :: Maybe String
+  , ssnStr :: Maybe String
+  , maritalStatusStr :: Maybe String
+  , phoneNumberStr :: Maybe String }
+
+data Person =
+  Person
+  { firstName :: FirstName
+  , lastName :: LastName
+  , ssn :: SocialSecurityNumber
+  , maritalStatus :: MaritalStatus
+  , phoneNumber :: PhoneNumber } 
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn "someFunc!"
